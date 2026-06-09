57296a347d74c4e04d93233a2bbee3083fae532f
ddwhitaker2 <dannydwhitaker2@gmail.com>
2026-06-09 03:27:29 -0500

    First Commit

---
+++ main.py
@@ -0,0 +1,4 @@
+# The goal of this project is to create a service that can scrap over job posting to find jobs and internships that would be beneficial for me to apply to.
+
+# To start this we should first define what a job is. Usually, we would create a class, but I want to dumb it down as much as possible so I can learn as much as possible.
+
